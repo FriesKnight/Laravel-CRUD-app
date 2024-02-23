3326f2a5bb56eebc90899e160524ae7c1cab100c
jonathan juining <73358104+FriesKnight@users.noreply.github.com>
2024-02-24 00:17:54 +0800

    update

--- My_first_CRUD/resources/views/components/layout.blade.php
+++ My_first_CRUD/resources/views/components/layout.blade.php
@@ -22,12 +22,12 @@
         },
       }
   </script>
-  <title>LaraMeme| Find Laravel Meme & Cookies</title>
+  <title>Pinteresting| Search & Upload images</title>
 </head>
 
 <body class="mb-48">
-  <nav class="flex justify-between items-center mb-4">
-    <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
+  <nav class="flex justify-between items-center mb-4 bg-teal-300 p-4">
+    <a href="/"><img class="w-32" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
     <ul class="flex space-x-6 mr-6 text-lg">
       @auth
       <li>
@@ -64,7 +64,7 @@
     class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
     <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
-    <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+    <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Upload Image!</a>
   </footer>
 
   <x-flash-message />
