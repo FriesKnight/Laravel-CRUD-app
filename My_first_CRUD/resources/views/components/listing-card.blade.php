04a3c971cd847038eb268efe6413e07ec47389b6
jonathan juining <73358104+FriesKnight@users.noreply.github.com>
2024-02-27 19:30:59 +0800

    update

--- My_first_CRUD/resources/views/components/listing-card.blade.php
+++ My_first_CRUD/resources/views/components/listing-card.blade.php
@@ -8,7 +8,7 @@
       <h3 class="text-2xl">
         <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
       </h3>
-      <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+      <div class="text-xl font-bold mb-4">By: {{auth()->user()->name}}</div>
       <x-listing-tags :tagsCsv="$listing->tags" />
       <div class="text-lg mt-4">
         <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
--- My_first_CRUD/resources/views/listings/show.blade.php
+++ My_first_CRUD/resources/views/listings/show.blade.php
@@ -10,7 +10,7 @@
           <h3 class="text-2xl mb-2">
             {{$listing->title}}
           </h3>
-          <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+          <div class="text-xl font-bold mb-4">by: {{auth()->user()->name}}</div>
   
           <x-listing-tags :tagsCsv="$listing->tags" />
   
@@ -19,18 +19,10 @@
           </div>
           <div class="border border-gray-200 w-full mb-6"></div>
           <div>
-            <h3 class="text-3xl font-bold mb-4">Job Description</h3>
+            <h3 class="text-3xl font-bold mb-4">About this Picture:</h3>
             <div class="text-lg space-y-6">
               {{$listing->description}}
   
-              <a href="mailto:{{$listing->email}}"
-                class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i
-                  class="fa-solid fa-envelope"></i>
-                Contact Employer</a>
-  
-              <a href="{{$listing->website}}" target="_blank"
-                class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i>
-                Visit Website</a>
             </div>
           </div>
         </div>
