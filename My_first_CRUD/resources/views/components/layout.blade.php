230132a9476e7d8abed5daf6e638500769c7a77e
jonathan juining <73358104+FriesKnight@users.noreply.github.com>
2024-02-28 10:12:04 +0800

    Revert "update"
    This reverts commit 04a3c971cd847038eb268efe6413e07ec47389b6.

--- My_first_CRUD/resources/views/components/layout.blade.php
+++ My_first_CRUD/resources/views/components/layout.blade.php
@@ -36,7 +36,7 @@
         </span>
       </li>
       <li>
-        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Images</a>
+        <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
       </li>
       <li>
         <form class="inline" method="POST" action="/logout">
--- My_first_CRUD/resources/views/listings/create.blade.php
+++ My_first_CRUD/resources/views/listings/create.blade.php
@@ -8,6 +8,14 @@
       <form method="POST" action="/listings" enctype="multipart/form-data">
         @csrf
         <div class="mb-6">
+          <label for="company" class="inline-block text-lg mb-2">Your Name</label>
+          <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
+            value="{{old('company')}}" />
+  
+          @error('company')
+          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+          @enderror
+        </div>
   
         <div class="mb-6">
           <label for="title" class="inline-block text-lg mb-2">Image Title</label>
@@ -20,9 +28,9 @@
         </div>
   
         <div class="mb-6">
-          <label for="location" class="inline-block text-lg mb-2">Location</label>
+          <label for="location" class="inline-block text-lg mb-2">Job Location</label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-            placeholder="Example: Malaysia, USA..." value="{{old('location')}}" />
+            placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}" />
   
           @error('location')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -30,11 +38,34 @@
         </div>
   
         <div class="mb-6">
+          <label for="email" class="inline-block text-lg mb-2">
+            Contact Email
+          </label>
+          <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
+  
+          @error('email')
+          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+          @enderror
+        </div>
+  
+        <div class="mb-6">
+          <label for="website" class="inline-block text-lg mb-2">
+            Website/Application URL
+          </label>
+          <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website"
+            value="{{old('website')}}" />
+  
+          @error('website')
+          <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+          @enderror
+        </div>
+  
+        <div class="mb-6">
           <label for="tags" class="inline-block text-lg mb-2">
-            Tags - Add Interesting tags for your image! (Comma seperated)
+            Tags (Comma Separated)
           </label>
           <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-            placeholder="Example: Aesthetic, Food, Figurines..." value="{{old('tags')}}" />
+            placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}" />
   
           @error('tags')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -43,7 +74,7 @@
   
         <div class="mb-6">
           <label for="logo" class="inline-block text-lg mb-2">
-            Your Image
+            Your Image 
           </label>
           <input type="file" class="border border-teal-200 rounded p-2 w-full" name="logo" />
   
@@ -54,10 +85,10 @@
   
         <div class="mb-6">
           <label for="description" class="inline-block text-lg mb-2">
-            Description
+            Job Description
           </label>
           <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-            placeholder="What is this picture about...?">{{old('description')}}</textarea>
+            placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
   
           @error('description')
           <p class="text-red-500 text-xs mt-1">{{$message}}</p>
